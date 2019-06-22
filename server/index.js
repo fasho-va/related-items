41ccf66c5f810194792c0e8f18e7f3d3ee2f6628
TheOriginalYangster <will.yang361@gmail.com>
2019-06-22 11:36:19 -0500

    committing before removing from local

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParcer = require('body-parser');
 const cors = require('cors');
-const PORT = 3005;
+const PORT =  process.env.PORT || 3005;
 const { getItemByUuid, getItemsByCategory, getRandomItems } = require('../db/index.js');
 
 const app = express();
